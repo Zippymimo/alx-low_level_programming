8c9c1b3b904b27aa9ebc97af298fecf98adfd4ac
Zippymimo <zippywairimo@gmail.com>
2023-03-16 15:04:06 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* Description; main - A programme that prints a line using printf function
+* Return; Always 0 (success)
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
