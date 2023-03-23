df8a22f1bd4475de5243814f5e8159a7b6f5228b
Zippymimo <zippywairimo@gmail.com>
2023-03-23 12:09:51 -0700

    positive or negative

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints integer as positive or negative
+ * Description: prints as positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+int i;
+
+i = 0;
+positive_or_negative(i);
+return (0);
+}
