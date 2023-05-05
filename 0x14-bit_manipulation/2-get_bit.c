8ae11eb40914af0071bf3346606236116f8eb856
Zippymimo <zippywairimo@gmail.com>
2023-05-05 00:55:10 -0700

    2. 10 bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -5,7 +5,7 @@
  * @n: The number to extract the bit from.
  * @index: The index of the bit to retrieve.
  *
- * Return: The value of the bit at the specified index, or -1 if an error occurs.
+ * Return: The value of the bit at the specified index, or -1 if error occurs.
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
