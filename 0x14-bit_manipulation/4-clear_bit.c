5a5980212bf1276f918a174ce512d4c1ffe74fd5
Zippymimo <zippywairimo@gmail.com>
2023-05-05 01:32:49 -0700

    100

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * get_bit - Returns the value of a bit at a given index.
+ * @n: The number to extract the bit from.
+ * @index: The index of the bit to retrieve.
+ *
+ * Return: 1 on success and  -1 on failure.
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int mask;
+
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+
+	mask = 1UL << index;
+	return ((n & mask) ? 1 : 0);
+}
