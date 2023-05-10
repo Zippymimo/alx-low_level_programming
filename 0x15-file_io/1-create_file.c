47860a08ce33cb506d758d746214c3e20ec38873
Zippymimo <zippywairimo@gmail.com>
2023-05-10 00:00:10 -0700

    Under the snows

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,23 +1,33 @@
 #include "main.h"
 
+/**
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
+ *
+ * Return: If the function fails - -1.
+ *         Otherwise - 1.
+ */
 int create_file(const char *filename, char *text_content)
 {
-    if (filename == NULL)
-        return -1;
+	int o, w, len = 0;
 
-    int file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-    if (file_descriptor == -1)
-        return -1;
+	if (filename == NULL)
+		return (-1);
 
-    if (text_content != NULL) {
-        ssize_t text_length = strlen(text_content);
-        ssize_t bytes_written = write(file_descriptor, text_content, text_length);
-        if (bytes_written != text_length) {
-            close(file_descriptor);
-            return -1;
-        }
-    }
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
 
-    close(file_descriptor);
-    return 1;
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+
+	return (1);
 }
